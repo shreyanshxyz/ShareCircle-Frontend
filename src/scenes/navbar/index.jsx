83d1f981fbad1a9554772bfb8d770d065a55f384
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-05-20 22:50:55 +0530

    CSS Modifications

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -40,7 +40,6 @@ const Navbar = () => {
   const alt = theme.palette.background.alt;
 
   const fullName = `${user.firstName} ${user.lastName}`;
-  // const fullName = "Nigger Singh";
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
